20661359485176ddfe0075c6dd6ec4554884e34f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-21 04:32:35 +0100

    create header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import "./App.css";
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className=" App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<div />} />
+      </Routes>
+    </div>
+  );
 };
-
 export default App;
