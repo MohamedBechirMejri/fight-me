87d14fdc52ab989e6bff85d7eb53073af0bab65e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-22 14:48:14 +0100

    style text

--- frontend/src/Components/Home/Section1-Banner.js
+++ frontend/src/Components/Home/Section1-Banner.js
@@ -3,11 +3,20 @@ import bannerBg from "../../images/banner-bg.png";
 const Banner = () => {
   return (
     <div
-      className="h-screen "
+      className="h-screen relative"
       style={{
         backgroundImage: `url(${bannerBg})`,
       }}
-    ></div>
+    >
+      <div className="absolute top-1/2 left-1/2 font-bold -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center text-center text-white gap-2">
+        <h3 className="text-[#00fadf] text-5xl ">PLAY UNLIMITED</h3>
+        <h1 className="text-7xl m-8">TOURNAMENTS</h1>
+        <p className="text-lg font-semibold">
+          Compete in Free and Paid entry Tournaments. Transform your games to
+          real money eSports
+        </p>
+      </div>
+    </div>
   );
 };
 
