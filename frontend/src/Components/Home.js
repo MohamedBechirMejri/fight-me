863af44a8b130e3ff37213e302002f108de21cbd
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-23 00:50:05 +0100

    add test images

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,30 @@
 import Banner from "./Home/Section1-Banner";
+import AvailableGames from "./Home/Section2-AvailableGames";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <AvailableGames
+        games={[
+          {
+            image: "https://picsum.photos/200/300",
+            title: "Game 1",
+          },
+          {
+            image: "https://picsum.photos/200/300",
+            title: "Game 2",
+          },
+          {
+            image: "https://picsum.photos/200/300",
+            title: "Game 3",
+          },
+          {
+            image: "https://picsum.photos/200/300",
+            title: "Game 4",
+          },
+        ]}
+      />
     </div>
   );
 };
