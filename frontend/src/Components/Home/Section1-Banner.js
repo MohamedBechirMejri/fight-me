c11b4a8bcfeaef5c265166042fa188aa4a121546
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-23 00:49:03 +0100

    tweak button position

--- frontend/src/Components/Home/Section1-Banner.js
+++ frontend/src/Components/Home/Section1-Banner.js
@@ -16,7 +16,7 @@ const Banner = () => {
       <div className="z-20 flex-col items-center justify-center gap-2 font-bold text-center text-white">
         <h3 className="text-[#00fadf] text-5xl ">PLAY UNLIMITED</h3>
         <h1 className="m-8 text-7xl">TOURNAMENTS</h1>
-        <p className="w-3/4 m-auto text-lg font-semibold">
+        <p className="w-3/4 m-auto mb-8 text-lg font-semibold">
           Compete in Free and Paid entry Tournaments. Transform your games to
           real money eSports
         </p>
