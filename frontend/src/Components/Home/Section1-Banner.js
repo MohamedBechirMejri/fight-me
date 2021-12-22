837ae62c0c7c0472ec1ced237f1c6a2e50bda104
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-22 15:46:53 +0100

    improve text

--- frontend/src/Components/Home/Section1-Banner.js
+++ frontend/src/Components/Home/Section1-Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
       <div className="absolute top-1/2 left-1/2 font-bold -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center text-center text-white gap-2">
         <h3 className="text-[#00fadf] text-5xl ">PLAY UNLIMITED</h3>
         <h1 className="text-7xl m-8">TOURNAMENTS</h1>
-        <p className="text-lg font-semibold">
+        <p className="text-lg font-semibold w-3/4 m-auto">
           Compete in Free and Paid entry Tournaments. Transform your games to
           real money eSports
         </p>
