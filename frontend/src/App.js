600dcb3d54e0aa7e50a33f41b597ffce09624678
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-23 20:36:38 +0100

    import login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,14 @@ import "./App.css";
 import Home from "./Components/Home";
 import React , {useContext} from "react";
 import { myContext } from "./AuthContext/context";
+import Login from "./Components/Login";
 const App = () => {
-  const  user  = useContext(myContext);
+  const user = useContext(myContext);
+  console.log(user);
   return (
     <div className=" App">
       <Header />
+      <Login/>
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
