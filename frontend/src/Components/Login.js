0fc3abbf36b6d0a901d8057d8298a0fd97e86978
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-23 21:23:19 +0100

    style login popup

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment, useRef, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react"; 
+import { Dialog, Transition } from "@headlessui/react";
 import { RedirectToDiscord, RedirectToGoogle } from "../api/apiCalls";
 
 const Login = () => {
@@ -45,13 +45,13 @@ const Login = () => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block overflow-hidden text-center align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
+            <div className="inline-block overflow-hidden text-center align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full ">
+              <div className="px-4 pt-5 pb-4 bg-[#0e063d] sm:p-6 sm:pb-4 ">
                 <div className="flex justify-center sm:flex sm:items-center">
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <Dialog.Title
                       as="h3"
-                      className="text-lg font-medium leading-6 text-center text-gray-900"
+                      className="text-3xl font-extrabold leading-6 text-center text-white"
                     >
                       Login
                     </Dialog.Title>
@@ -59,10 +59,10 @@ const Login = () => {
                   </div>
                 </div>
               </div>
-              <div className="justify-center px-4 py-3 text-white bg-gray-50 sm:px-6 sm:flex sm:flex-row">
+              <div className="justify-center px-4 py-3 text-white bg-gray-50 sm:px-6 sm:flex sm:flex-row transition-all">
                 <button
                   type="button"
-                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium    border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm bg-[#EA4335]"
+                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base    border border-gray-300 rounded-md shadow-sm hover:bg-gray-50   sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm bg-[#EA4335] hover:text-[#EA4335] transition-all  font-bold outline-none focus:outline-none"
                   onClick={() => {
                     RedirectToGoogle();
                     setOpen(false);
@@ -73,7 +73,7 @@ const Login = () => {
                 </button>{" "}
                 <button
                   type="button"
-                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium   border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm bg-[#5865F2]"
+                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base   border border-gray-300 rounded-md shadow-sm hover:bg-gray-50   sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm bg-[#5865F2] hover:text-[#5865F2] transition-all  font-bold outline-none"
                   onClick={() => {
                     RedirectToDiscord();
                     setOpen(false);
@@ -84,7 +84,7 @@ const Login = () => {
                 </button>{" "}
                 <button
                   type="button"
-                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base font-medium    border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm bg-[#1877F2]"
+                  className="inline-flex justify-center w-full px-4 py-2 mt-3 text-base    border border-gray-300 rounded-md shadow-sm hover:bg-gray-50   sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm bg-[#1877F2] hover:text-[#1877F2] transition-all  font-bold outline-none"
                   onClick={() => setOpen(false)}
                   ref={cancelButtonRef}
                 >
