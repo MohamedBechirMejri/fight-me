a42f0790827ffa75198ea523787282333e2d827f
ali jandoubi <alijandhd@gmail.com>
2021-12-23 01:37:37 +0100

    implementing Oauth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import "./App.css";
 import Home from "./Components/Home";
+import React , {useContext} from "react";
+import { myContext } from "./AuthContext/context";
 const App = () => {
+  const  user  = useContext(myContext);
   return (
     <div className=" App">
       <Header />
