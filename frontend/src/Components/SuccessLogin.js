27cbcdc41efa0f317a44884933ff42cd69131d78
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-23 22:12:42 +0100

    reload page after login success

--- frontend/src/Components/SuccessLogin.js
+++ frontend/src/Components/SuccessLogin.js
@@ -1,31 +1,27 @@
 import axios from "axios";
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 
 const fetchuser = async () => {
-    const response = await axios
-    .get("http://localhost:3030/getuser", {withCredentials : true})
-    .catch(err=>{
+  const response = await axios
+    .get("http://localhost:3030/getuser", { withCredentials: true })
+    .catch((err) => {
       console.log(err);
       window.location("/");
       console.log("not allowed");
-    //   location.href("/");
+      window.location.href("/");
     });
-    if (response && response.data) {
-        localStorage.removeItem('user');
-        localStorage.setItem("user", JSON.stringify(response.data));
-
-
-    }
+  if (response && response.data) {
+    localStorage.removeItem("user");
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
+};
 function SucessLogin() {
-
-    useEffect (()=>{
-        fetchuser();
-        setTimeout(() => {
-            window.close()
-        }, 1000);
-
-    },[]);
-    return <div>Logged in</div>
+  useEffect(() => {
+    fetchuser();
+    setTimeout(() => {
+      window.close();
+    }, 1000);
+  }, []);
+  return <div>Logged in</div>;
 }
 export default SucessLogin;
