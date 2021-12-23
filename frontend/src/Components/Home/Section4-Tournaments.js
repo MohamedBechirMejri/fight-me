9093d2f77905e5cd552c167fcae8a46bfae89c05
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-23 16:55:00 +0100

    add background image to section 4

--- frontend/src/Components/Home/Section4-Tournaments.js
+++ frontend/src/Components/Home/Section4-Tournaments.js
@@ -3,7 +3,12 @@ import tournamentsBg from "../../assets/images/tournaments-bg.png";
 
 const Tournaments = (props) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 p-8 text-white">
+    <div
+      className="flex flex-col items-center justify-center gap-4 p-8 text-white "
+      style={{
+        backgroundImage: `url(${tournamentsBg})`,
+      }}
+    >
       <h1>BROWSE TOURNAMENTS</h1>
       <p>
         Find the perfect tournaments for you. Head to head matches where you
