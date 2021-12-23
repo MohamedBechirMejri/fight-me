ef2289dcc320b7e10015081dd99acef4d3ca510f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-23 20:08:27 +0100

    add some styling

--- frontend/src/Components/Home/Section4-Tournaments.js
+++ frontend/src/Components/Home/Section4-Tournaments.js
@@ -9,12 +9,12 @@ const Tournaments = (props) => {
         backgroundImage: `url(${tournamentsBg})`,
       }}
     >
-      <h1>BROWSE TOURNAMENTS</h1>
-      <p>
+      <h1 className="mt-20 font-bold text-7xl">BROWSE TOURNAMENTS</h1>
+      <p className="w-1/3 text-xl text-center">
         Find the perfect tournaments for you. Head to head matches where you
         pick the game, rules and prize.
       </p>
-      <div>
+      <div className="flex ">
         {props.tournaments.map((tournament) => (
           <Tournament key={tournament.title} tournament={tournament} />
         ))}
