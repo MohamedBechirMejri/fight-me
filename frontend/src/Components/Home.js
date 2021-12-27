11d1d7a479a9771102ccfe83cd1296f09eb16801
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-27 16:00:39 +0100

    remove unused import

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,12 +1,9 @@
-import { get_game_list } from "../api/apiCalls";
 import Banner from "./Home/Section1-Banner";
 import AvailableGames from "./Home/Section2-AvailableGames";
 import HowItWorks from "./Home/Section3-HowItWorks";
 import Tournaments from "./Home/Section4-Tournaments";
 
-
 const Home = () => {
- 
   return (
     <div>
       <Banner />
