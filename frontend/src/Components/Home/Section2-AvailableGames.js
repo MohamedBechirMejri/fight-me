6b953fa0300f1d4d3eea932e722021e7811f1df3
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-27 15:40:39 +0100

    format code

--- frontend/src/Components/Home/Section2-AvailableGames.js
+++ frontend/src/Components/Home/Section2-AvailableGames.js
@@ -4,34 +4,33 @@ import Button from "../SubComponents/Button";
 import Game from "./Game";
 
 const AvailableGames = (props) => {
-  const [game_list,SetGame_list]=useState([]);
-  const [loading,SetLoading]=useState(false);
+  const [game_list, SetGame_list] = useState([]);
+  const [loading, SetLoading] = useState(false);
   const get_game_list = async () => {
     try {
-      const games_api = "https://aqueous-falls-70675.herokuapp.com/api/v1/collect/games/GamesAvailable";
-    axios.get(games_api).then((response) => {
-      if (response.status === 201) {
-        SetGame_list(response.data.results);
-       var games = {
-         games:response.data.results
-       };
-        localStorage.setItem("games", JSON.stringify(games));
-       
-        SetLoading(true);
-      }else{
-        console.log(response.data.errors);
-      }
-    })
+      const games_api =
+        "https://aqueous-falls-70675.herokuapp.com/api/v1/collect/games/GamesAvailable";
+      axios.get(games_api).then((response) => {
+        if (response.status === 201) {
+          SetGame_list(response.data.results);
+          var games = {
+            games: response.data.results,
+          };
+          localStorage.setItem("games", JSON.stringify(games));
+
+          SetLoading(true);
+        } else {
+          console.log(response.data.errors);
+        }
+      });
     } catch (error) {
       console.log(error);
     }
-    
   };
-useEffect (()=>{
-  get_game_list()
-}, []);
+  useEffect(() => {
+    get_game_list();
+  }, []);
   return (
-    
     <div className="flex items-center justify-center w-screen h-screen">
       <div className="flex flex-col w-[50vw] h-[70vh] bg-gradient-to-b from-[#4609C3] to-[#0E063D] rounded-3xl p-20 text-white gap-8">
         <div className="flex items-center justify-between w-full">
@@ -40,14 +39,24 @@ useEffect (()=>{
             <p className="text-lg ">We are constantly adding new games</p>
           </div>
         </div>
-        <img src =""></img>
-        
 
-          {loading ? (<div className="flex items-center justify-center gap-4">{game_list.map((game) => (
-            <Game key={game.title} title={game.title} image={game.image} />
-          ))}</div>) : (<div className="flex items-center justify-center gap-4"><img className="" style={{maxWidth:"100px",maxHeight:"100px"}} src="https://cutewallpaper.org/24/loading-gif-png/loadinggifpng5-superior-lawn-care.png"></img></div>)}
-          
-        
+        {loading ? (
+          <div className="flex items-center justify-center gap-4 overflow-scroll">
+            {game_list.map((game) => (
+              <Game key={game.title} title={game.title} image={game.image} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex items-center justify-center gap-4">
+            <img
+              className=""
+              style={{ maxWidth: "100px", maxHeight: "100px" }}
+              src="https://cutewallpaper.org/24/loading-gif-png/loadinggifpng5-superior-lawn-care.png"
+              alt="change this later"
+            ></img>
+          </div>
+        )}
+
         <Button name="VIEW ALL" />
       </div>
     </div>
