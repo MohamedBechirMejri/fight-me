de31fda9ee25a81071784710b95bad9cfd976d3a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-27 15:45:34 +0100

    import swiper stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import  Context  from "./AuthContext/context";
+import Context from "./AuthContext/context";
+import "swiper/css/bundle";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination, Navigation } from "swiper";
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation]);
+
 ReactDOM.render(
   <React.StrictMode>
-      <Context>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-      </Context>
+    <Context>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Context>
   </React.StrictMode>,
   document.getElementById("root")
 );
