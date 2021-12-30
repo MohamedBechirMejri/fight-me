58a9d32a09c8dcee3e2d6551780e02c686bb0af5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-30 03:15:02 +0100

    style players of the week component

--- frontend/src/Components/Home/Section6-PlayersOfTheWeek.js
+++ frontend/src/Components/Home/Section6-PlayersOfTheWeek.js
@@ -1,80 +1,100 @@
 import Button from "../SubComponents/Button";
 import { Link } from "react-router-dom";
 import playersWeekBg from "../../assets/images/players-week-bg.png";
+import topLevelBg from "../../assets/images/top-level.png";
+import playersCircle from "../../assets/images/players-circle.png";
 
 const PlayersOfTheWeek = () => {
   const players = [
+    {
+      name: "Lebron James",
+      image: "https://picsum.photos/200/200",
+      rank: 2,
+      points: "17,947",
+      reward: "+30 credits",
+      profileLink: "/players/lebron-james",
+      lvl: "10",
+    },
     {
       name: "Kobe Bryant",
       image: "https://picsum.photos/200/200",
       rank: 1,
       points: "27,947",
-      reward: "+20 credits",
+      reward: "+50 credits",
       profileLink: "/player/kobe-bryant",
+      lvl: "15",
     },
-    {
-      name: "Lebron James",
-      image: "https://picsum.photos/200/200",
-      rank: 2,
-      points: "27,947",
-      reward: "+20 credits",
-      profileLink: "/players/lebron-james",
-    },
+
     {
       name: "Stephen Curry",
       image: "https://picsum.photos/200/200",
       rank: 3,
-      points: "27,947",
-      reward: "+20 credits",
+      points: "7,947",
+      reward: "+10 credits",
       profileLink: "/players/stephen-curry",
-    },
-    {
-      name: "Kevin Durant",
-      image: "https://picsum.photos/200/200",
-      rank: 4,
-      points: "27,947",
-      reward: "+20 credits",
-      profileLink: "/players/kevin-durant",
-    },
-    {
-      name: "James Harden",
-      image: "https://picsum.photos/200/200",
-      rank: 5,
-      points: "27,947",
-      reward: "+20 credits",
-      profileLink: "/players/james-harden",
+      lvl: "04",
     },
   ];
   return (
     <div
-      className="h-screen w-screen "
+      className="h-screen w-screen text-white flex flex-col items-center justify-center gap-12 text-center"
       style={{
         backgroundImage: `url(${playersWeekBg})`,
       }}
     >
-      <h1>Players of the week</h1>
-      <p>
+      <h1 className="text-7xl font-bold">Players of the week</h1>
+      <p className=" w-1/3 text-lg font-semibold">
         We take a look at the best player of the week awarded on Monday for the
         previous Monday to Sunday
       </p>
-      <div>
+      <div className="flex items-center gap-12">
         {players.map((player) => {
           return (
-            <div className="flex flex-col items-center justify-center">
-              <div>
+            <div
+              className={
+                player.rank === 1
+                  ? "flex flex-col items-center justify-center p-8 border border-[#EDB8FF] rounded-3xl gap-7  bg-[#0e063d80] -mt-8"
+                  : "flex flex-col items-center justify-center p-8 border border-[#EDB8FF] rounded-3xl gap-7 bg-[#0e063d80] "
+              }
+              style={{
+                "box-shadow": "rgb(237 184 255) 0px 0px 10px",
+              }}
+            >
+              <div
+                className="flex flex-col items-center justify-center"
+                style={{
+                  backgroundImage: `url(${playersCircle})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  height: "12em",
+                }}
+              >
                 <img
                   src={player.image}
                   alt={player.name + " image"}
-                  className="rounded-full w-1/3 ring-[1.5em] m-8 ring-[#4609C3]"
+                  className="rounded-full w-1/2 mt-6"
                 />
                 <div>
-                  <h2 className="text-4xl font-bold">{player.rank}</h2>
+                  <h2
+                    className="text-xl  p-3 -mt-8"
+                    style={{
+                      backgroundImage: `url(${topLevelBg})`,
+                      backgroundSize: "contain",
+                      backgroundPosition: "center",
+                      backgroundRepeat: "no-repeat",
+                    }}
+                  >
+                    {player.lvl}
+                  </h2>
                 </div>
               </div>
 
-              <h2 className="text-4xl font-bold">{player.name}</h2>
-              <p className="text-[#64BE56]">{player.points}</p>
-              <p className="text-gray-600">{player.reward}</p>
+              <h2 className="text-2xl font-bold">{player.name}</h2>
+              <p className="text-[#64BE56] font-semibold">
+                {player.points} XP Earned
+              </p>
+              <p className="text-[#00FADF]  font-semibold">{player.reward}</p>
               <Link to={player.profileLink}>
                 <Button name="VIEW PROFILE" />
               </Link>
